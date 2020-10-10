87ca7d92220db72b95fadc288d67a9d60718419e
renxinmei <renxinmei@wakedata.com>
2020-10-10 17:16:14 +0800

    fix：新增侧边栏图标

--- newGraduClient/src/components/Sidebar.vue
+++ newGraduClient/src/components/Sidebar.vue
@@ -6,26 +6,70 @@
         <el-menu class="sidebar" :unique-opened="true" :default-active="currentPagePath" @open="handleOpen"
                  :router="true" @close="handleClose">
             <el-menu-item index="/dashboard/welcome">
-                <i class="fa fa-tachometer"></i>
+                <i class="fa icon-zhuye"></i>
                 <span>后台主页</span>
+            </el-menu-item>
+             <el-menu-item index="/dashboard/order">
+                <i class="icon-icon"></i>
+                <span>订单列表</span>
             </el-menu-item>
             <el-submenu index="goods">
                 <template slot="title">
-                    <i class="fa fa-shopping-bag"></i>
+                    <i class="fa icon-shangpin"></i>
                     <span>商品管理</span>
                 </template>
                 <el-menu-item index="/dashboard/goods">
-                    <i class="fa fa-circle"></i>
+                    <!-- <i class="fa fa-circle"></i> -->
                     <span>商品列表</span>
                 </el-menu-item>
                 <el-menu-item index="/dashboard/nature">
-                    <i class="fa fa-circle"></i>
+                    <!-- <i class="fa fa-circle"></i> -->
                     <span>商品设置</span>
                 </el-menu-item>
             </el-submenu>
-            
+
+            <el-menu-item index="/dashboard/user">
+                <i class="fa fa-large icon-1"></i>
+                <span>用户列表</span>
+            </el-menu-item>
+            <el-submenu index="settings">
+                <template slot="title">
+                    <i class="fa fa-large icon-dianpu-copy"></i>
+                    <span>店铺设置</span>
+                </template>
+                <el-menu-item index="/dashboard/settings/showset">
+                    <!-- <i class="fa fa-circle"></i> -->
+                    <span>显示设置</span>
+                </el-menu-item>
+                <el-menu-item index="/dashboard/ad">
+                    <!-- <i class="fa fa-circle"></i> -->
+                    <span>广告列表</span>
+                </el-menu-item>
+                <el-menu-item index="/dashboard/notice">
+                    <!-- <i class="fa fa-circle"></i> -->
+                    <span>公告管理</span>
+                </el-menu-item>
+                <el-menu-item index="/dashboard/freight">
+                    <!-- <i class="fa fa-circle"></i> -->
+                    <span>运费模板</span>
+                </el-menu-item>
+                <el-menu-item index="/dashboard/shipper">
+                    <!-- <i class="fa fa-circle"></i> -->
+                    <span>快递设置</span>
+                </el-menu-item>
+                <el-menu-item index="/dashboard/admin">
+                    <!-- <i class="fa fa-circle"></i> -->
+                    <span>管理员</span>
+                </el-menu-item>
+            </el-submenu>
+
+             <el-menu-item index="/dashboard/shopcart">
+                <i class="fa fa-large icon-gouwucheman"></i>
+                <span>购物车</span>
+            </el-menu-item>
+
             <el-menu-item @click="logout">
-                <i class="fa fa-large fa-sign-out"></i>
+                <i class="fa fa-large icon-tuichu"></i>
                 <span>退出</span>
             </el-menu-item>
         </el-menu>
--- newGraduClient/src/main.js
+++ newGraduClient/src/main.js
@@ -8,6 +8,7 @@ import Axios from 'axios'
 import VueAxios from 'vue-axios'
 import 'element-ui/lib/theme-chalk/index.css'
 import store from './store'
+import '@/assets/icon/iconfont.css'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI);
