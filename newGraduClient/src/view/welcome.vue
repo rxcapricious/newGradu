965391a4dc39911a615fcc1e6964c94aaee92552
renxinmei <renxinmei@wakedata.com>
2020-10-20 17:44:58 +0800

    fix:welcome页面搭建完成

--- newGraduClient/src/view/welcome.vue
+++ newGraduClient/src/view/welcome.vue
@@ -13,21 +13,129 @@
             <div class="header">
                 <el-row :gutter="12">
                     <el-col :span="8">
-                        <el-card shadow="never">
-                        总是显示
+                        <el-card class="cardItem itemRed"  shadow="never">
+                        <a href="">
+                            <h1>34</h1>
+                            <div>惶惶不可终日</div>
+                        </a>
                         </el-card>
                     </el-col>
                     <el-col :span="8">
-                        <el-card shadow="never">
-                        鼠标悬浮时显示
+                        <el-card class="cardItem itemGreen" shadow="never">
+                         <a href="">
+                            <h1>34</h1>
+                            <div>惶惶不可终日</div>
+                        </a>
                         </el-card>
                     </el-col>
                     <el-col :span="8">
-                        <el-card shadow="never">
-                        从不显示
+                        <el-card class="cardItem itemGray" shadow="never">
+                         <a href="">
+                            <h1>34</h1>
+                            <div>惶惶不可终日</div>
+                        </a>
                         </el-card>
                     </el-col>
                 </el-row>
+            </div>
+
+            <div class="main">
+                <el-tabs v-model="activeName" type="card" @tab-click="handleClick">
+                    <el-tab-pane label="今天" name="first"></el-tab-pane>
+                    <el-tab-pane label="昨天" name="second"></el-tab-pane>
+                    <el-tab-pane label="最近7天" name="third"></el-tab-pane>
+                    <el-tab-pane label="最近30天" name="fourth"></el-tab-pane>
+                </el-tabs>
+                <div class="tab-content">
+                  <el-row :gutter="12">
+                    <el-col :span="8">
+                        <el-card class="cardItem2">
+                            <div slot="header">
+                                <span style="line-height:36px">顾客</span>
+                                <el-popover>
+                                    啦啦啦！！！
+                                </el-popover>
+                            </div>
+                            <div class="text item">
+                                <span>新增</span>
+                                <h3 style="flost: right;">2</h3>
+                            </div>
+                            <div class="text item">
+                                <span>老顾客</span>
+                                <h3 style="flost: right;">2</h3>
+                            </div>
+                        </el-card>
+                     </el-col>
+
+                     <el-col :span="8">
+                         <el-card class="cardItem2">
+                            <div slot="header">
+                                <span style="line-height:36px">下单</span>
+                            </div>
+                            <div class="text item">
+                                <span>加入购物车</span>
+                                <h3 style="flost: right;">2</h3>
+                            </div>
+                            <div class="text item">
+                                <span>提交订单数/金额</span>
+                                <h3 style="flost: right;">2</h3>
+                            </div>
+                        </el-card>
+                     </el-col>
+
+                    <el-col :span="8">
+                        <el-card class="cardItem2">
+                            <div slot="header">
+                                <span style="line-height:36px">支付</span>
+                            </div>
+                            <div class="text item">
+                                <span>成交订单数</span>
+                                <h3 style="flost: right;">2</h3>
+                            </div>
+                            <div class="text item">
+                                <span>成交金额</span>
+                                <h3 style="flost: right;">2</h3>
+                            </div>
+                        </el-card>
+                     </el-col>
+                  </el-row>
+                </div>
+
+                <div class="line"></div>
+
+                <div class="block-4">
+                    <el-card class="box-card">
+                        <div class="text item">
+                            <span>客单价</span>
+                            <p style="float: right;">222</p>
+                        </div>
+                        <p class="tips">成交金额/成交订单数</p>
+                    </el-card>
+
+                     <el-card class="box-card">
+                        <div class="text item">
+                            <span>下单转化率</span>
+                            <p style="float: right;">222</p>
+                        </div>
+                        <p class="tips">下单人数/访问人数</p>
+                    </el-card>
+
+                    <el-card class="box-card">
+                        <div class="text item">
+                            <span>下单-支付转化率</span>
+                            <p style="float: right;">222</p>
+                        </div>
+                        <p class="tips">支付人数/下单人数</p>
+                    </el-card>
+
+                    <el-card class="box-card">
+                        <div class="text item">
+                            <span>支付转化率</span>
+                            <p style="float: right;">222</p>
+                        </div>
+                        <p class="tips">支付人数/访问人数</p>
+                    </el-card>
+                </div>
             </div>
         </div>
     </div>
@@ -41,6 +149,10 @@ export default {
 
 <style lang="less" scoped>
 .wel-content {
+    a {
+        text-decoration: none;
+        color: black;
+    }
     .notice{
         width: 100%;
         height: 60px;
@@ -83,6 +195,57 @@ export default {
     }
     .header {
         margin-bottom: 30px;
+    }
+    .cardItem {
+        text-align: left;
+        margin: 0 20px 14px 0;
+        h1 {
+            color: #fff;
+            margin-bottom: 10px;
+        }
+        div {
+            color: #fff;
+            margin-bottom: 10px;
+        }
+    }
+
+
+    .itemRed {
+        background-color: #e64242;
+    }
+    .itemGreen {
+        background-color: #11b95c;
+    }
+    .itemGray {
+        background-color: #1f2d3d;
+    }
+
+
+    .cardItem2 {
+        text-align: left;
+        margin: 0 20px 14px 0;
+    }
+    .text {
+        font-size: 14px;
+        padding: 10px 0;
+        display: flex;
+        justify-content: space-between;
+    }
+
+
+    .line {
+        margin: 20px 0;
+        border-top: 1px solid #d1dbe5;
+    }
+    .box-card {
+        width: 32%;
+        float: left;
+        margin: 0 20px 14px 0;
+    }
+    .tips {
+        color: #8c939d;
+        font-size: 13px;
+        text-align: left;
     }
 }
 </style>
